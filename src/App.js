597c2bd41e4cbf0ad8ef630baacfeec7c7daf21c
talhasiddiquixd <talhasiddiquixd@gmail.com>
2023-01-27 18:45:08 +0500

    react-Helmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import axios from 'axios';
 
 // import { Row } from 'antd';
@@ -21,26 +21,42 @@ const ScoopRank = () => {
   const link =
     'http://https://63bd-39-53-106-137.in.ngrok.io/scooprank:3000/ScoopRank/';
   return (
-    <div style={{ backgroundColor: 'white', height: '100vh' }}>
-      {' '}
-      <div>
-        <Helmet>
-          <meta charSet='utf-8' />
+    // <div style={{ backgroundColor: 'white', height: '100vh' }}>
+    //   {' '}
+    //   <div>
+    //     <Helmet>
+    //       <meta charSet='utf-8' />
 
-          <meta
-            name='description'
-            content={data?.body}
-          />
-          <title>{data?.title}</title>
-          <link
-            rel='canonical'
-            href={link}
-          />
-        </Helmet>
-        <h1>{data?.title}</h1>
-        <h1>{data?.body}</h1>
-      </div>
-    </div>
+    //       <meta
+    //         name='description'
+    //         content={data?.body}
+    //       />
+    //       <title>{data?.title}</title>
+    //       <link
+    //         rel='canonical'
+    //         href={link}
+    //       />
+    //     </Helmet>
+    //     <h1>{data?.title}</h1>
+    //     <h1>{data?.body}</h1>
+    //   </div>
+    // </div>
+
+    <HelmetProvider>
+      <Helmet>
+        <title>Buy Me A Car</title>
+        <link
+          rel='canonical'
+          href='https://www.tacobell.com/'
+        />
+        <meta
+          name='Hello there'
+          content='sdbjhklm '
+        />
+      </Helmet>
+      <h1>{data.title}</h1>
+      <h1>{data.body}</h1>
+    </HelmetProvider>
   );
 };
 export default ScoopRank;
