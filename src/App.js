25c2128bc5b54cb938cc0ebaa7809e16f3682a9d
talhasiddiquixd <talhasiddiquixd@gmail.com>
2023-01-27 17:10:06 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import axios from 'axios';
 
-function App() {
+// import { Row } from 'antd';
+
+const ScoopRank = () => {
+  const [data, setData] = useState([]);
+  const headers = {};
+
+  let val = true;
+  useEffect(() => {
+    if (val) {
+      const baseURL = 'https://jsonplaceholder.typicode.com/posts/1';
+      axios.get(baseURL, {}).then((response) => {
+        setData(response.data);
+      });
+      val = false;
+    }
+  }, []);
+  const link =
+    'http://https://63bd-39-53-106-137.in.ngrok.io/scooprank:3000/ScoopRank/';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: 'white', height: '100vh' }}>
+      {' '}
+      <div>
+        <Helmet>
+          <meta charSet='utf-8' />
+
+          <meta
+            name={data?.title}
+            content={data?.body}
+            image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvNPKubvc49DDL6siEuD8nk_01eLY38gpieXjJ14mETw&s'
+          />
+          <title>{data?.title}</title>
+          <link
+            rel='canonical'
+            href={link}
+          />
+        </Helmet>
+        <h1>{data?.title}</h1>
+        <h1>{data?.body}</h1>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
+export default ScoopRank;
