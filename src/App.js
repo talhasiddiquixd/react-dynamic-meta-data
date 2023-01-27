95ef01aba8549b6c605d2abd3bd28a40cceb5333
talhasiddiquixd <talhasiddiquixd@gmail.com>
2023-01-27 19:13:32 +0500

    car

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet';
 import axios from 'axios';
 
 // import { Row } from 'antd';
-
 const ScoopRank = () => {
   const [data, setData] = useState([]);
   const headers = {};
@@ -15,32 +14,13 @@ const ScoopRank = () => {
       axios.get(baseURL, {}).then((response) => {
         setData(response.data);
       });
+
       val = false;
     }
   }, []);
   const link =
     'http://https://63bd-39-53-106-137.in.ngrok.io/scooprank:3000/ScoopRank/';
   return (
-    // <div style={{ backgroundColor: 'white', height: '100vh' }}>
-    //   {' '}
-    //   <div>
-    //     <Helmet>
-    //       <meta charSet='utf-8' />
-
-    //       <meta
-    //         name='description'
-    //         content={data?.body}
-    //       />
-    //       <title>{data?.title}</title>
-    //       <link
-    //         rel='canonical'
-    //         href={link}
-    //       />
-    //     </Helmet>
-    //     <h1>{data?.title}</h1>
-    //     <h1>{data?.body}</h1>
-    //   </div>
-    // </div>
     <>
       <Helmet>
         <meta charSet='utf-8' />
